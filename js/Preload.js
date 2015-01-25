9c55a6f072e5098da21296cd315286a05624f097
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-01-24 14:26:55 -1000

    adding gun, samcat, and preload for samcat only

--- js/Preload.js
+++ js/Preload.js
@@ -19,7 +19,7 @@ Dogvasion.Preload.prototype = {
  
     //load game assets
     this.load.image('ground', 'assets/images/sprites/ground.jpg', 400, 200);
-    this.load.spritesheet('kittens', 'assets/images/sprites/kittens.png', 32, 32, 96);
+    this.load.spritesheet('kittens', 'assets/images/sprites/samcat.png', 50, 50, 9);
     // this.load.spritesheet('cat', 'assets/images/sprites/Grizzo_Cat.png');
     // this.load.spritesheet('dude', 'assets/images/sprites/dude.png', 32, 48);
  
